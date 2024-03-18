4fd79fd90f75f1f19f0f8efae49a6c3f3e062306
mantuonweb <mantuonweb@hotmail.com>
2024-03-18 14:21:02 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,6 +18,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angularbamb');
   });
 
+  it(`should have as title 'angularbamb'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('angularbamb');
+  });
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
